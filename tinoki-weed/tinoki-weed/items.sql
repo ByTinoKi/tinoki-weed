58192421b0084af27a9d3d2fdd3c6b58325f5444
ByTinoKi <dfatinoki@gmail.com>
2022-07-26 19:56:42 +0200

    tinoki-weed

---
+++ tinoki-weed/tinoki-weed/items.sql
@@ -0,0 +1,6 @@
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES  
+    ('item_weed', 'Weed', 1),
+    ('zip_bag', 'ZIP Bag', 1),
+    ('scissors', 'Scissors', 1),
+    ('packed_weed', 'Weed bag', 5)
+;
